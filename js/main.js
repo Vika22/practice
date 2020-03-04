041f801879ef3bb9c880d7db3dce70b9f8d790e7
Vika22 <vika.tkachenko22@gmail.com>
2020-03-04 17:59:16 +0200

    add last

--- js/main.js
+++ js/main.js
@@ -5,15 +5,19 @@ let buttonChange = document.querySelector(".list__input-name");
 let counter = 0;
 button.onclick = function () {
     counter++;  
-    text = document.createElement("span");
-    text.innerHTML +=  counter +  ". " + input.value +  "<br/>";
+    text = document.createElement("p");
+    text.innerHTML +=  counter +  ". " + input.value ;
     document.getElementById("list").reset();
     document.getElementsByTagName("div")[0].appendChild(text); 
-};
 
+    let btn = document.createElement("BUTTON");
+    btn.innerHTML = "Delete";
+    document.getElementsByTagName("div")[0].appendChild(btn);
+
+};
 buttonChange.onclick = function () {
-    let elem = document.querySelector("span");
-    elem.remove();
+    BUTTON = document.querySelector("span");
+    BUTTON.remove();
   
 };
 
