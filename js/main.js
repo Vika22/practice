8697ce501fd34ee6dee8b8e5f2b6ac551c765e2c
Vika22 <vika.tkachenko22@gmail.com>
2020-03-06 11:02:21 +0200

    do

--- js/main.js
+++ js/main.js
@@ -1,53 +1,40 @@
 let text = document.querySelector(".list-text");
 let input = document.querySelector(".input-text");
 let button = document.querySelector(".input-button");
-let counter = 0;
 button.onclick = function () {
     if (input.value != 0) {
 
-        counter++;
-        text = document.createElement("p");
-        text.classList.add("par");
-        text.innerHTML += counter + ". " + input.value + " ";
-        document.getElementsByTagName("div")[0].appendChild(text);
-        document.getElementById('myInput').value = '';
+        let list = document.createElement("li");
+        text.appendChild(list);
 
-        let search = document.getElementsByClassName("par");
-        if (search != 0) {
-            for (let i = 0; i < search.length; i++) {
-                search[i].onclick = function () {
-                    let del = document.createElement("button");
-                    del.innerHTML = "Delete";
-                    document.body.appendChild(del);
-                    del.onclick = function () {
-                        counter--;
-                        search[i].remove();
-                        edit.remove();
-                        del.remove();
-                        this.remove();
-                    }
+        let someList = document.createElement("p");
+        someList.innerHTML = input.value;
+        list.appendChild(someList);
 
-                    let edit = document.createElement("button");
-                    edit.innerHTML = "Edit";
-                    document.body.appendChild(edit);
-                    edit.onclick = function () {
-                        let editInput = document.createElement("input");
-                        editInput.value = text.textContent;
-                        document.body.appendChild(editInput);
-                        editInput.addEventListener('change', updateValue);
-
-                        function updateValue(editInput) {
-                            search[i].textContent = this.value;
-                            edit.remove();
-                            del.remove();
-                            this.remove();
-
-                        }
-                    }
-                }
-            }
-
+        let del = document.createElement("button");
+        del.innerHTML = "Delete";
+        list.appendChild(del);
+        del.onclick = function () {
+           text.removeChild(list);  
         }
+        let edit = document.createElement("button");
+        edit.innerHTML = "Edit";
+        list.appendChild(edit);
+        edit.onclick = function () {
+            let editInput = document.createElement("input");
+            editInput.type = "text";
+            list.appendChild(editInput);
+            edit = this;
+            editInput.value = someList.innerHTML;
+            editInput.addEventListener('change', updateValue);
+            function updateValue() {
+                someList.innerHTML = this.value;
+                editInput.remove();
+            }   
+        }   
+       input.value = '';   
     }
-
 };
+
+
+
