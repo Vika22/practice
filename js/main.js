45f2714c55260f40575008dc8a78a96ad58bd656
Vika22 <vika.tkachenko22@gmail.com>
2020-03-06 10:51:02 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,62 +1,37 @@
-let addField = document.getElementById('add');
-let inputText = document.getElementById('text');
-let list = document.getElementById('list');
+let text = document.querySelector(".list-text");
+let input = document.querySelector(".input-text");
+let button = document.querySelector(".input-button");
+button.onclick = function () {
+    if (input.value != 0) {
 
+        let list = document.createElement("li");
+        text.appendChild(list);
 
-addField.onclick = function () {
-  if (inputText.value != 0) {
-    listItem = createNewElement(inputText.value);
-    list.appendChild(listItem);
-    linkEvents(listItem);
-    inputText.value = "";
-    input.value = "";
+        let someList = document.createElement("p");
+        someList.innerHTML = input.value;
+        list.appendChild(someList);
 
-  };
-};
-
-function createNewElement(text) {
-  let listItem = document.createElement('li');
-  let label = document.createElement('label');
-  label.innerHTML = text;
-  listItem.appendChild(label);
-
-  let deleteButton = document.createElement('button');
-  deleteButton.innerHTML = 'delete';
-  deleteButton.classList.add('delete');
-  listItem.appendChild(deleteButton);
-
-
-  let editButton = document.createElement('button');
-  editButton.innerHTML = 'edit';
-  editButton.classList.add('edit');
-  listItem.appendChild(editButton);
-
-  let input = document.createElement('input');
-  input.type = "text";
-  listItem.appendChild(input);
-
-
-  return listItem;
-
-}
-
-function linkEvents() {
-  deleteButton = listItem.querySelector('button.delete');
-  deleteButton.onclick = function () {
-    listItem = this.parentNode;
-    let ul = listItem.parentNode;
-    ul.removeChild(listItem);
-  }
-  editButton = listItem.querySelector('button.edit');
-  editButton.onclick = function () {
-    editButton = this;
-    listItem = this.parentNode;
-    label = listItem.querySelector('label');
-    input = listItem.querySelector('input');
-    input.value = label.innerHTML;
-    input.addEventListener('change', updateValue);
-    function updateValue(input) {
-      label.textContent = this.value;
+        let del = document.createElement("button");
+        del.innerHTML = "Delete";
+        list.appendChild(del);
+        del.onclick = function () {
+           text.removeChild(list);  
+        }
+        let edit = document.createElement("button");
+        edit.innerHTML = "Edit";
+        list.appendChild(edit);
+        edit.onclick = function () {
+            let editInput = document.createElement("input");
+            editInput.type = "text";
+            list.appendChild(editInput);
+            edit = this;
+            editInput.value = someList.innerHTML;
+            editInput.addEventListener('change', updateValue);
+            function updateValue() {
+                someList.innerHTML = this.value;
+                editInput.remove();
+            }   
+        }   
+       input.value = '';   
     }
-  }
-}
+};
