ec1fbfbd3aa47eb07e97c0241c68c57c2e822f7c
Vika22 <vika.tkachenko22@gmail.com>
2020-03-09 23:09:35 +0200

    Update main.js
    ol

--- js/main.js
+++ js/main.js
@@ -13,26 +13,26 @@ button.addEventListener("click", function () {
 function Add() {
 
     let list = document.createElement("li");
-    list.classList.add('text-left');
+    list.classList.add('text-left', 'text-light', 'border-bottom', 'border-light', 'mr-5');
     text.appendChild(list);
     let checkbox = document.createElement("input");
     checkbox.classList.add('checkbox');
     checkbox.type = "checkbox";
     list.appendChild(checkbox);
     let someList = document.createElement("label");
-    someList.classList.add('someList');
+    someList.classList.add('someList', 'text-light');
     someList.innerHTML = input.value;
     someList.classList.add('someList');
     list.appendChild(someList);
     let edit = document.createElement("button");
-    edit.classList.add('edit');
+    edit.classList.add('edit', 'm-2', 'bg-info', 'text-light', 'border-light');
     edit.innerHTML = "Edit";
-    list.appendChild(edit); 
+    list.appendChild(edit);
     let del = document.createElement("button");
-    del.classList.add('delete');
+    del.classList.add('delete', 'm-2', 'bg-info', 'text-light', 'border-light');
     del.innerHTML = "Delete";
     list.appendChild(del);
-    
+
     return list;
 };
 function Delete() {
@@ -50,35 +50,36 @@ function Edit() {
         }
         edit = this;
         list = this.parentNode;
-        let editType = document.createElement("p");
-        editType.classList.add('editInput');
-        list.appendChild(editType);
+
         let editInput = document.createElement("input");
         editInput.type = "text";
-        editType.appendChild(editInput);
+        editInput.classList.add('editInput', 'bg-info', 'text-light', 'border-light');
+        list.appendChild(editInput);
         someList = list.querySelector('label.someList');
         editInput.value = someList.innerHTML;
         let save = document.createElement("button");
+        save.classList.add('ml-2', 'bg-info', 'text-light', 'border-light');
         save.innerHTML = "Save";
-        editType.appendChild(save);
+        list.appendChild(save);
         save.onclick = function () {
             someList.innerHTML = editInput.value;
-            editType.remove();
+            editInput.remove();
+            save.remove();
         }
     }
 };
 function Check() {
     someList = list.querySelector('label.someList');
     let isChecked = false;
-    someList.addEventListener("click", function (ev) {
+    someList.addEventListener("click", function () {
         someList = this;
         list = this.parentNode;
-        if (isChecked === false){
+        if (isChecked === false) {
             checkbox = list.querySelector('input.checkbox');
             checkbox.setAttribute("checked", "checked");
             someList.style.textDecoration = "line-through";
             isChecked = true;
-        }else{
+        } else {
             isChecked = false;
             someList.style.textDecoration = "none";
             checkbox.removeAttribute("checked");
