dfe1030928a2773422654b85ad601767e51d32de
Vika22 <vika.tkachenko22@gmail.com>
2020-03-10 11:45:56 +0200

    all

--- js/main.js
+++ js/main.js
@@ -6,32 +6,46 @@ button.addEventListener("click", function () {
         list = Add();
         Delete();
         Edit();
-        Check();
+        // Check();
         input.value = '';
     }
 });
 function Add() {
 
     let list = document.createElement("li");
-    list.classList.add('text-left', 'text-light', 'border-bottom', 'border-light', 'mr-5');
+    list.classList.add('text-left', 'text-light', 'd-flex', 'border-bottom', 'border-light', 'mr-5', 'mb-2');
     text.appendChild(list);
+
+    let customControl = document.createElement("div");
+    customControl.classList.add('custom-control', 'custom-checkbox');
+    list.appendChild(customControl);
+
+
     let checkbox = document.createElement("input");
-    checkbox.classList.add('checkbox');
+    checkbox.classList.add('checkbox', 'custom-control-input');
+  checkbox.setAttribute("id", "customControlAutosizing");
     checkbox.type = "checkbox";
-    list.appendChild(checkbox);
+    customControl.appendChild(checkbox);
+
     let someList = document.createElement("label");
-    someList.classList.add('someList', 'text-light');
+    someList.classList.add('someList', 'text-light', 'mt-2','custom-control-label');
     someList.innerHTML = input.value;
+    
+    someList.setAttribute("for", "customControlAutosizing");
     someList.classList.add('someList');
-    list.appendChild(someList);
+    customControl.appendChild(someList);
+
     let edit = document.createElement("button");
-    edit.classList.add('edit', 'm-2', 'bg-info', 'text-light', 'border-light');
+    edit.classList.add('edit', 'ml-2', 'mb-2',  'btn', 'btn-outline-info', 'active');
     edit.innerHTML = "Edit";
     list.appendChild(edit);
     let del = document.createElement("button");
-    del.classList.add('delete', 'm-2', 'bg-info', 'text-light', 'border-light');
-    del.innerHTML = "Delete";
+    del.classList.add('delete', 'ml-2', 'mb-2', 'close', 'btn', 'btn-info');
     list.appendChild(del);
+    let close = document.createElement("span");    
+    close.setAttribute('aria-hidden', 'true');
+    close.innerHTML = '&times';
+    del.appendChild(close);
 
     return list;
 };
@@ -51,16 +65,28 @@ function Edit() {
         edit = this;
         list = this.parentNode;
 
+        let inputGroup = document.createElement("div");
+        inputGroup.classList.add('input-group', 'mb-2');
+        list.appendChild(inputGroup);
+
         let editInput = document.createElement("input");
         editInput.type = "text";
-        editInput.classList.add('editInput', 'bg-info', 'text-light', 'border-light');
-        list.appendChild(editInput);
+        editInput.classList.add('editInput', 'form-control');
+        inputGroup.appendChild(editInput);
+
         someList = list.querySelector('label.someList');
         editInput.value = someList.innerHTML;
-        let save = document.createElement("button");
-        save.classList.add('ml-2', 'bg-info', 'text-light', 'border-light');
-        save.innerHTML = "Save";
-        list.appendChild(save);
+
+        let inputGroupAppend = document.createElement("div");
+        inputGroupAppend.classList.add('input-group-append');
+        inputGroup.appendChild(inputGroupAppend);
+
+        let save = document.createElement("input");
+        save.type = "button";
+        save.classList.add( 'btn', 'btn-outline-info', 'active');
+        save.value = "Save";
+        inputGroupAppend.appendChild(save);
+
         save.onclick = function () {
             someList.innerHTML = editInput.value;
             editInput.remove();
@@ -68,21 +94,22 @@ function Edit() {
         }
     }
 };
-function Check() {
-    someList = list.querySelector('label.someList');
-    let isChecked = false;
-    someList.addEventListener("click", function () {
-        someList = this;
-        list = this.parentNode;
-        if (isChecked === false) {
-            checkbox = list.querySelector('input.checkbox');
-            checkbox.setAttribute("checked", "checked");
-            someList.style.textDecoration = "line-through";
-            isChecked = true;
-        } else {
-            isChecked = false;
-            someList.style.textDecoration = "none";
-            checkbox.removeAttribute("checked");
-        }
-    })
-};
+// function Check() {
+//     customControl = list.querySelector('input.customControl');
+//     someList = customControl.querySelector('label.someList');
+//     let isChecked = false;
+//     someList.addEventListener("click", function () {
+//         someList = this;
+//         list = this.parentNode;
+//         if (isChecked === false) {
+//             checkbox = customControl.querySelector('input.checkbox');
+         
+//             someList.style.textDecoration = "line-through";
+//             isChecked = true;
+//         } else {
+//             isChecked = false;
+//             someList.style.textDecoration = "none";
+//             checkbox.removeAttribute("id");
+//         }
+//     })
+// };
