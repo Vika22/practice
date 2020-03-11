fe0580dd8ad9055c02588743ce1e7e737c761b35
Vika22 <vika.tkachenko22@gmail.com>
2020-03-11 11:59:40 +0200

    huraay

--- js/main.js
+++ js/main.js
@@ -1,17 +1,52 @@
+Vue.component('task', {
+    name: 'Task',
+    props: ['taskItem'],
+    template: `
+        <li class="text-left d-flex mr-5 mb-2">
+            <template v-if="todoType === 'change'">
+            <div class="checkbox"  :class="{'done': taskItem.checked }" >
+                <label class="text-info mt-2">
+                <input v-model="taskItem.checked" type="checkbox">
+                    {{taskItem.title}}</label>
+            </div>
+            </template>
+            <template v-else>
+                <label class="text-info mt-2"> 
+                    {{taskItem.title}}</label>
+                <input class="form-control" v-model="taskItem.title">
+            </template>
+            <button @click="changeTodo" class="edit ml-2 mb-2 btn btn-outline-info active">Edit</button>
+            <button @click="$emit('on-delete')" class="delete ml-2 mb-2 close btn btn-info">
+                <span aria-hidden="true">×</span>
+            </button>
+        </li>
+    `,
+    data: () => {
+        return {
+            todoType: 'change'
+        }
+    },
+    methods: {
+        changeTodo: function () {
+            this.todoType = this.todoType === 'change' ? 'input' : 'change'
+        }
+    }
+})
+
 var app17 = new Vue({
     el: '#todo-list',
-    data: {
-        title: 'TO DO list',
-        newTodoText: '',
-        todos: [
-        ],
-        nextTodoId: 0,
-        editedTodo: 0
+    data: () => {
+        return {
+            title: 'TO DO list',
+            newTodoText: '',
+            todos: [
+            ],
+            nextTodoId: 0
+        }
     },
     methods: {
         addTodo: function () {
             if (!this.newTodoText) {
-
                 return;
             }
             this.todos.push({
@@ -20,6 +55,9 @@ var app17 = new Vue({
                 checked: false
             })
             this.newTodoText = ''
+        },
+        deleteTodo (index) {
+            this.todos.splice(index, 1)
         }
     }
 })
